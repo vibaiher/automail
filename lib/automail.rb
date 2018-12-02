c23ae00c54b09056cf12c8177c4d21f45da0626b
Vicente Baixauli <vibaiher@gmail.com>
2018-12-02 16:16:10 +0100

    Add base template

--- lib/automail.rb
+++ lib/automail.rb
@@ -1,6 +1,23 @@
-require "automail/version"
+require 'automail/version'
 
 module Automail
   class Error < StandardError; end
-  # Your code goes here...
+
+  def self.generate()
+    <<~TXT
+      Hola {name},
+
+      Para saber más de Devscola lo mejor es que te pases a la siguiente asamblea que será el {date} a las {time}, así puedes conocernos a todos, ver como funcionamos y podremos entender mejor que es lo que necesitas.
+
+      Estamos en:
+
+      La Cueva de Benimaclet
+      Carrer de Francesc Martínez 2, 46020 València
+      https://goo.gl/maps/HAnZfTCTBo52
+
+      ##### MENSAJE ORIGINAL #####
+
+      {original_message}
+    TXT
+  end
 end
--- spec/automail_spec.rb
+++ spec/automail_spec.rb
@@ -14,4 +14,28 @@ RSpec.describe Automail do
       expect(output).to include(expected_output)
     end
   end
+
+  describe 'as an automatic mailer tool' do
+    it 'has a base template mail' do
+      email = Automail.generate()
+
+      expect(email).to eq(
+        <<~TXT
+          Hola {name},
+
+          Para saber más de Devscola lo mejor es que te pases a la siguiente asamblea que será el {date} a las {time}, así puedes conocernos a todos, ver como funcionamos y podremos entender mejor que es lo que necesitas.
+
+          Estamos en:
+
+          La Cueva de Benimaclet
+          Carrer de Francesc Martínez 2, 46020 València
+          https://goo.gl/maps/HAnZfTCTBo52
+
+          ##### MENSAJE ORIGINAL #####
+
+          {original_message}
+        TXT
+      )
+    end
+  end
 end
